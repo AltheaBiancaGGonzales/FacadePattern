e318b97afffdcbf9976564b983df277c953b5b27
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-27 11:01:01 +0800

    Update Light.java

--- Light.java
+++ Light.java
@@ -1,1 +1,10 @@
+public class Light implements HomeService {
+    public void turnOn() {
+        System.out.println("Lights are ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Lights are OFF");
+    }
+}
 
