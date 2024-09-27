0d0e0eeea225a48b89d110b77bfdcb4948b32d36
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-27 11:01:18 +0800

    Update Tv.java

--- Tv.java
+++ Tv.java
@@ -1,1 +1,10 @@
+public class TV implements HomeService {
+    public void turnOn() {
+        System.out.println("TV is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("TV is OFF");
+    }
+}
 
