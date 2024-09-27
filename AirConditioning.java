e0afb2c6a2bfc2221efd062fd50d56ba01684d83
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-27 11:01:33 +0800

    Update AirConditioning.java

--- AirConditioning.java
+++ AirConditioning.java
@@ -1,1 +1,10 @@
+public class AirConditioning implements HomeService {
+    public void turnOn() {
+        System.out.println("Air Conditioning is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Air Conditioning is OFF");
+    }
+}
 
